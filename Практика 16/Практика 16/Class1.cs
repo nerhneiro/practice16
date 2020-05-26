974ba8bf60ffde7826d96ba720b014bc28154310
nerhneiro <63853354+nerhneiro@users.noreply.github.com>
2020-05-26 11:05:20 +0300

    Prakrika 16 Variant 5

---
+++ Практика 16/Практика 16/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_16
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            n = 0;
+        }
+        public Class1(int a)
+        {
+            n = a;
+            d = "";
+        }
+        int n;
+        string d;
+        public int convert_to_ten(string Text, int Length)
+        {
+            int ten = 0, k = 0, nn;
+            nn = n;
+            for (int i = 0; i < Length; i++)
+            {
+                if (n % 10 == 1) ten += (int)Math.Pow(2, k);
+                k++;
+                n = n / 10;
+            }
+            if (int.Parse(Text) < 0) ten *= -1;
+            n = nn;
+            return ten;
+        }
+        public int convert_to_eight (int n, string Text, int Length)
+        {
+            int ten = 0, k = 0, nn;
+            nn = n;
+            while (n > 0)
+            {
+                ten = ten * 10 + n % 8;
+                n = n / 8;
+            }
+            k = ten;
+            ten = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (k != 0) ten = ten * 10 + k % 10;
+                k = k / 10;
+            }
+            if (int.Parse(Text) < 0) ten  *= -1;
+            n = nn;
+            return ten;
+        }
+        public string convert_to_six(int n, string Text)
+        {
+            string ei;
+            n = (int)Math.Abs(n);
+            if (n == 0) d = "0";
+            while (n > 0)
+            {
+                if (n % 16 == 10) ei = "A";
+                else if (n % 16 == 11) ei = "B";
+                else if (n % 16 == 12) ei = "C";
+                else if (n % 16 == 13) ei = "D";
+                else if (n % 16 == 14) ei = "E";
+                else if (n % 16 == 15) ei = "F";
+                else ei = (n % 16).ToString();
+                d += ei;
+                n = n / 16;
+            }
+            ei = "";
+            for (int i = d.Length - 1; i >=0; i--)
+            {
+                ei += d[i];
+            }
+            if (int.Parse(Text) < 0) ei = "-" + ei;
+            return ei;
+        }
+    }
+}
---
+++ Практика 16/Практика 16/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        string text1 = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            string ei;
+            text1 = textBox1.Text;
+            try
+            {
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    if (textBox1.Text[i] != '0' && textBox1.Text[i] != '1' && textBox1.Text[i] != '-') { throw new Exception(": Вы ввели не двоичное число"); }
+                }
+                Class1 obj = new Class1(((int)Math.Abs(Convert.ToInt32(textBox1.Text))));
+                if (radioButton2.Checked == true)
+                {
+                    n = obj.convert_to_ten(textBox1.Text, textBox1.Text.Length);
+                    textBox2.Text = "Число " + textBox1.Text + " в десятичном виде = " + n;
+                }
+                if (radioButton1.Checked == true)
+                {
+                    n = (int)Math.Abs(obj.convert_to_ten(textBox1.Text, textBox1.Text.Length));
+                    textBox2.Text = "Число " + textBox1.Text + " в 8 системе счисления = " + obj.convert_to_eight(n, textBox1.Text, textBox1.Text.Length);
+                }
+                if (radioButton3.Checked == true)
+                {
+                    n = obj.convert_to_ten( textBox1.Text, textBox1.Text.Length);
+                    ei = obj.convert_to_six(n, textBox1.Text);
+                    textBox2.Text = "Число " + textBox1.Text + " в 16 системе счисления = " + ei;
+                }
+            }
+            catch (Exception err)
+            {
+                textBox2.Text = "Неправильный формат ввода " + err.Message;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (text1 != textBox1.Text) { textBox2.Text = ""; text1 = textBox1.Text; }
+        }
+    }   
+}
